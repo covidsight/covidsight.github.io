69fc98525d4e44704369974ccb69ec4e84128e7b
gasparramoa <ramoawork@gmail.com>
2021-04-28 23:38:52 +0200

    TopBar modifications. Java script and CSS as well

--- js/main.js
+++ js/main.js
@@ -290,3 +290,74 @@
 
 
 }());
+
+
+// Get the container element
+var btnContainer = document.getElementById("colorlib-menubar");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("a");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+
+
+jQuery(document).ready(function(jQuery) {            
+	var topMenu = jQuery("#top-menu"),
+		offset = 40,
+		topMenuHeight = topMenu.outerHeight()+offset,
+		// All list items
+		menuItems =  topMenu.find('a[href*="#"]'),
+		// Anchors corresponding to menu items
+		scrollItems = menuItems.map(function(){
+		  var href = jQuery(this).attr("href"),
+		  id = href.substring(href.indexOf('#')),
+		  item = jQuery(id);
+		  //console.log(item)
+		  if (item.length) { return item; }
+		});
+
+	// so we can get a fancy scroll animation
+	menuItems.click(function(e){
+	  var href = jQuery(this).attr("href"),
+		id = href.substring(href.indexOf('#'));
+		  offsetTop = href === "#" ? 0 : jQuery(id).offset().top-topMenuHeight+1;
+	  jQuery('html, body').stop().animate({ 
+		  scrollTop: offsetTop
+	  }, 300);
+	  e.preventDefault();
+	});
+
+	// Bind to scroll
+	jQuery(window).scroll(function(){
+	   // Get container scroll position
+	   var fromTop = jQuery(this).scrollTop()+topMenuHeight;
+
+	   // Get id of current scroll item
+	   var cur = scrollItems.map(function(){
+		 if (jQuery(this).offset().top < fromTop)
+		   return this;
+	   });
+
+	   // Get the id of the current element
+	   cur = cur[cur.length-1];
+	   var id = cur && cur.length ? cur[0].id : "";               
+	   
+	   menuItems.parent().removeClass("active");
+	   if(id){
+			menuItems.parent().end().filter("[href*='#"+id+"']").parent().addClass("active");
+	   }
+	   
+	})
+})
+
+
+
